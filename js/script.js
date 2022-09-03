c59a3a21d17ff457509c59ceb10c7afd8795f8b8
Elena Liubimova <elena.liubimova3@gmail.com>
2022-09-03 18:40:05 +0300

    feat: first version

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let submitButton = document.querySelector('.form__submit-button');
+/*let resetButton = document.querySelector('.form__reset-button');
+let inputAge = document.getElementById(age);
+let inputHeight = document.getElementById(height);
+let inputWeight = document.getElementById(weight);
+let inputsGroup = document.querySelector('.inputs-group');*/
+
+/*let inputs = inputsGroup.querySelectorAll('input');
+console.log(inputs);
+console.log('Hello world!');*/
+
